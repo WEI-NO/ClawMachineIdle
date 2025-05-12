12f80ec031d7ab1176941c034b5df558eafd9f86
weinowu <arnoldwu2023@gmail.com>
2025-05-11 17:21:42 -0700

    Color Changes
    Edited Sprites and Visual

--- Assets/Scripts/Gameplay/GrabButton.cs
+++ Assets/Scripts/Gameplay/GrabButton.cs
@@ -1,34 +1,10 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class GrabButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+
+public class GrabButton : InGameButton
 {
-    private Animator animator;
-
-    [Header("Animator Triggers")]
-    public string downTrigger = "Pressed";
-    public string startDownTrigger = "Down";
-
-    private void Awake()
+    protected override void ButtonFunction()
     {
-        animator = GetComponent<Animator>();
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        animator.SetBool(downTrigger, true);
-        animator.SetTrigger(startDownTrigger);
-
         ClawObject.Instance.StartGrabSequence();
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        animator.SetBool(downTrigger, false);
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        // Optional: handle dragging visuals if needed
     }
 }
---
+++ Assets/Scripts/Gameplay/RefreshButton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+public class RefreshButton : InGameButton
+{
+    protected override void ButtonFunction()
+    {
+        print("Refresh");
+    }
+}
