c7378c80011fe89b855c66357c00c2be293e3a62
weinowu <arnoldwu2023@gmail.com>
2025-08-24 16:39:56 -0700

    Base Openable UI
    Added a base Openable UI for UI that can be toggled on and off with buttons

--- Assets/Scripts/UI/BagUI.cs
+++ Assets/Scripts/UI/BagUI.cs
@@ -1,25 +1,5 @@
 using UnityEngine;
 
-public class BagUI : MonoBehaviour
+public class BagUI : OpenableUI
 {
-    private Animator anim;
-    public bool active = false;
-
-
-    void Awake()
-    {
-        anim = GetComponent<Animator>();        
-    }
-
-    public void Toggle()
-    {
-        active = !active;
-        if (active)
-        {
-            anim.SetTrigger("Open");
-        } else
-        {
-            anim.SetTrigger("Close");
-        }
-    }
 }
