c695bb9340e5ea48ab6d77b5d9546e7cea643202
weinowu <arnoldwu2023@gmail.com>
2025-09-08 15:05:35 -0700

    Incubator Display
    - Added new changes to the incubator display

---
+++ Assets/Scripts/UI/IncubatorDisplay.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class IncubatorDisplay : OpenableUI
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
